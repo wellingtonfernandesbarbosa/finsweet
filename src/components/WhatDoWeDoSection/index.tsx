6acb555e6a4fd53040925dbaeed8294821679f7c
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2024-08-27 18:43:46 -0300

    Translate What Do We Do Section

--- src/components/WhatDoWeDoSection/index.tsx
+++ src/components/WhatDoWeDoSection/index.tsx
@@ -5,38 +5,21 @@ import Pray from "./assets/pray.svg";
 import Donate from "./assets/donate.svg";
 import Stream from "./assets/stream.svg";
 import { ThemeColor } from "../../enum/ThemeColor";
-import { Label2 } from "../Label";
 import { Heading2 } from "../Heading";
 
 export default function WhatDoWeDoSection() {
   return (
     <section className={styles.container}>
       <div className={styles.container__header}>
-        <Label2>Sub-Headline</Label2>
-        <Heading2 theme={ThemeColor.black}>a church that's relevant</Heading2>
+        <Heading2 theme={ThemeColor.black}>uma igreja que é relevante</Heading2>
       </div>
 
       <div className={styles.container__cards}>
-        <WelcomeCard
-          figure={Pray}
-          altFigure="Pray"
-          title="About us"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, tempora?"
-        />
+        <WelcomeCard figure={Pray} altFigure="Ícone de Orar" title="Sobre Nós" text="Descubra nossa história e missão. Conheça nossa comunidade e como fazemos a diferença na vida das pessoas." />
 
-        <WelcomeCard
-          figure={Stream}
-          altFigure="Stream"
-          title="Get Involved"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, tempora?"
-        />
+        <WelcomeCard figure={Stream} altFigure="Ícone de Transmitir" title="Participe" text="Acompanhe nossos cultos online, eventos e grupos de estudo. Conecte-se com outros membros da igreja." />
 
-        <WelcomeCard
-          figure={Donate}
-          altFigure="Donate"
-          title="Giving Back"
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, tempora?"
-        />
+        <WelcomeCard figure={Donate} altFigure="Ícone de Doar" title="Doe" text="Sua doação é fundamental para que possamos continuar nosso trabalho. Ajude-nos a levar a palavra de Deus a mais pessoas." />
       </div>
     </section>
   );
