0957c82ddd5c0c117b32d6d84838bcf16d999caa
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-01-08 07:49:21 -0300

    Translate and refactor BlogPostsSection in Home page

--- src/components/BlogPostsSection/index.tsx
+++ src/components/BlogPostsSection/index.tsx
@@ -5,54 +5,16 @@ import { PostCard } from "../Cards";
 import { Heading2 } from "../Heading";
 import { Label2 } from "../Label";
 
-interface BlogPost {
-  title: string;
-  description: string;
-  author: string;
-  date: string;
-}
-
-const blogPosts: BlogPost[] = [
-  {
-    title: "WATCH AND LISTEN TO OUR SERMONS",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-    author: "By Mathew Johnson",
-    date: "Tuesday 13 May, 2021",
-  },
-  {
-    title: "WATCH AND LISTEN TO OUR SERMONS",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-    author: "By Mathew Johnson",
-    date: "Tuesday 13 May, 2021",
-  },
-  {
-    title: "WATCH AND LISTEN TO OUR SERMONS",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-    author: "By Mathew Johnson",
-    date: "Tuesday 13 May, 2021",
-  },
-  {
-    title: "WATCH AND LISTEN TO OUR SERMONS",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
-    author: "By Mathew Johnson",
-    date: "Tuesday 13 May, 2021",
-  },
-];
+import posts from "../../data/blogPosts.json";
 
 export default function BlogPostsSection() {
   return (
     <section className={styles.container}>
-      <Label2>Read our Blog</Label2>
-      <Heading2 theme={ThemeColor.black}>SHARE, INSPIRE, INNOVATE</Heading2>
+      <Label2>Leia nosso Blog</Label2>
+      <Heading2 theme={ThemeColor.black}>COMPARTILHE, INSPIRE, INOVE</Heading2>
 
       <ul className={styles.container__cards}>
-        {blogPosts.map((blogPost, index) => (
-          <PostCard key={index} {...blogPost} bgColor={true} />
-        ))}
+        {posts.map((blogPost, index) => index < 4 && <PostCard key={index} {...blogPost} bgColor={true} />)}
       </ul>
     </section>
   );
